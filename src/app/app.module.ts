e564811434b6eb2136a9b089e13f188272b2d03c
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-05-05 11:15:17 -0300

    modificações da aula 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { PhotosModule } from './photos/photos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
