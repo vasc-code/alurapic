26e7879e74497a2fb3e2caaa066cc8ca03e7618b
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-05-11 17:31:14 -0300

    versão final do curso 3 de angular

--- src/app/home/signup/signup.service.ts
+++ src/app/home/signup/signup.service.ts
@@ -1,8 +1,9 @@
 import { NewUser } from './new-user';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
-const API_URL = "http://localhost:3000";
+const API = environment.ApiUrl;
 
 @Injectable()
 export class SignUpService {
@@ -11,10 +12,10 @@ export class SignUpService {
 
     checkUserNameTaken(userName: string) {
 
-        return this.http.get(API_URL + '/user/exists/' + userName);
+        return this.http.get(API + '/user/exists/' + userName);
     }
 
     signup(newUser: NewUser) {
-      return this.http.post(API_URL + '/user/signup', newUser);
+      return this.http.post(API + '/user/signup', newUser);
   }
 }
